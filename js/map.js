e437d9083f8a18ab847a59b1b0c8352fd587029b
joo hyoung joo <98748916+hyoungjoo0510@users.noreply.github.com>
2022-08-12 17:01:26 +0900

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -42,7 +42,7 @@ $(function(){
 
         var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-        var imageSrc = '../images/ktmmap.png', // 마커이미지의 주소입니다    
+        var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
             imageSize = new kakao.maps.Size(64, 32), // 마커이미지의 크기입니다
             imageOption = { offset: new kakao.maps.Point(32, 44) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
